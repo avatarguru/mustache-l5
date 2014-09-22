9fed7f8ee4a2ac553c76749b9dd93043b63a21ea
avatarguru <mcdimaculangan@gmail.com>
2014-09-23 01:04:18 +0400

    Updated MustacheL5ServiceProvider

--- src/Avatarguru/MustacheL5/MustacheL5ServiceProvider.php
+++ src/Avatarguru/MustacheL5/MustacheL5ServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class MustacheL4ServiceProvider extends ServiceProvider {
+class MustacheL5ServiceProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
